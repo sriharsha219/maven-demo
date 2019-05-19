5fb9f4999db6f3cc15eaeb16ef6995ff13882fca
EC2 Default User <ec2-user@ip-172-31-24-160.us-east-2.compute.internal>
2019-05-19 22:48:28 +0000

    Ap.java - commented scanner code

--- src/main/java/personal/App.java
+++ src/main/java/personal/App.java
@@ -9,10 +9,10 @@ public class App
 {
     public static void main( String[] args )
     {
-	Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome!" );
-	System.out.println("What is your name: ");
-	String name = sc.nextLine();
-	System.out.println("Hi, "+name);
+//	Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome user!" );
+//	System.out.println("What is your name: ");
+//	String name = sc.nextLine();
+//	System.out.println("Hi, "+name);
     }
 }
